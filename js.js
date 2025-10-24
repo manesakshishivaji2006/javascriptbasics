0b60fb0ed8633e1cdbd08f54f7a00cb47da3a3ea
sakshi mane <manesakshishivaji2006@gmail.com>
2025-10-24 19:35:52 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+console.log("sakshi")
+/* loops for, while, do while
+for loop - for(initial, condition,updation)
+{
+
+}  */
+for (let i=1; i<=10; i++)
+    {
+    console.log("sakshi");
+}
+// counting
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+// reverse counting
+for (let i= 5; i>=0; i--)
+{
+    console.log(i);
+}
+// continue - skip that itration and print reamining
+
