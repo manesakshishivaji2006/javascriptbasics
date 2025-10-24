3e49d527b52a035c12dd84f8265be3aa59f2111a
sakshi mane <manesakshishivaji2006@gmail.com>
2025-10-24 19:34:17 +0530

    Add files via upload

---
+++ arry.js
@@ -0,0 +1,31 @@
+// let obj ={
+//     name:"saskhi",
+//     age:23,
+//     weight:57,
+//     greet: function(){
+//         console.log("hello and how are you");
+//     }
+
+
+// };
+// console.log(obj);
+// obj.greet();
+// console.log(typeof(obj))
+
+// //build in methods
+// let arr =[ 1,2,3,4,,5];
+// let brr = new Array('java',1,true);
+
+// brr.push('sakshi');
+// brr.pop('sakshi')
+// console.log(brr)
+// brr.push(30)
+// brr.shift();
+// console.log(brr)
+
+
+let arr = [10,3,30,20,5,0];
+let ans =arr.map(number) =>
+{
+    return number*number;
+}
